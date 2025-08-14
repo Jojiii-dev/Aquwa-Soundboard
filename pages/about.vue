548b16392d5168361e09d7637fc65d2ca16772f9
georgeg308@hotmail.com <jojiii.dev@gmail.com>
2025-08-14 22:34:59 +1200

    initial commit

---
+++ pages/about.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <VCard>
+      <VCardTitle>
+        <span class="text-3xl">Aquwa Soundboard</span>
+      </VCardTitle>
+      <VCardText class="text-lg">
+        <ul class="list-disc pl-6">
+          <p>
+            Thanks for visiting my little soundboard site. I hope you enjoyed
+            it.
+          </p>
+          <li>
+            Created by:
+            <a class="link" href="https://x.com/Jojiii_dev" target="_blank">
+              Jojiii
+            </a>
+          </li>
+          <li>
+            Template originally made by:
+            <a class="link" href="https://konnokai.me/" target="_blank">
+              孤之界
+            </a>
+            ,
+            <a class="link" href="https://twitter.com/7Red4" target="_blank">
+              紅柿
+            </a>
+            ,
+            <a
+              class="link"
+              href="https://github.com/kujyonatsume"
+              target="_blank"
+            >
+              九条夏目
+            </a>
+          </li>
+          <li>Sound file Sources: Twitch Clip, Youtube clips</li>
+          <li>Contributors for soundbites: Jojiii</li>
+        </ul>
+      </VCardText>
+    </VCard>
+  </div>
+</template>
+<script setup lang="ts"></script>
+
+<style scoped>
+.link {
+  @apply text-primary-500 underline;
+}
+
+ul * {
+  @apply text-lg;
+}
+</style>
